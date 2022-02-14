960d8d88a7f4aa693dd664457693e244a0b1564b
IvanSkrebatovich <ivan.skrebatovich@gmail.com>
2022-02-14 19:33:05 +0300

    Updates
    -Created create doctor method

--- force-app/main/default/lwc/appointmentApp/appointmentApp.js
+++ force-app/main/default/lwc/appointmentApp/appointmentApp.js
@@ -1,9 +1,13 @@
-import { LightningElement, wire } from 'lwc';
-import getDoctors from '@salesforce/apex/ControllerDoctor.getDoctors';
+import { LightningElement, wire } from "lwc";
+import getDoctors from "@salesforce/apex/ControllerDoctor.getDoctors";
 
 export default class AppointmentApp extends LightningElement {
     listDoctor;
+    newDocName;
+    newDocStart;
+    newDocEnd;
     displayDiv = false;
+
     @wire(getDoctors) wiredDoctors;
     // @wire(getDoctors) wiredDoctors({data, error}) {
     //     if (data) {
@@ -12,27 +16,39 @@ export default class AppointmentApp extends LightningElement {
     //         console.log(error);
     //     }
     // }
+
+    //----------Shows/Hides doctor's create template----------
     showTemplateHandler() {
-        this.displayDiv = true;
+        this.displayDiv = !this.displayDiv;
     }
-    
+    //----------Put doctor's name and working hours in variables----------
+    docNameHandler(event) {
+        this.newDocName = event.target.value; 
+    }
 
-    value = 'choose Doctor';
+    hoursStartHandler(event) {
+        this.newDocStart = event.target.value;
+    }
 
+    hoursEndHandler(event) {
+        this.newDocEnd = event.target.value;
+    }
+
+    //----------This should be dropdown list of doctors----------
+    value = "choose Doctor";
     get options() {
         return [
-            { label: 'Doctor1', value: 'Doctor1' },
-            { label: 'Doctor2', value: 'Doctor2' },
-            { label: 'Doctor3', value: 'Doctor3' },
-            
+            { label: "Doctor1", value: "Doctor1" },
+            { label: "Doctor2", value: "Doctor2" },
+            { label: "Doctor3", value: "Doctor3" }
         ];
     }
 
     handleChange(event) {
         this.value = event.detail.value;
     }
-    handleClick (){
-        
-    console.log(this.wiredDoctors.data);
-}
+    //----------Button to check features/console.log ----------
+    handleClick() {
+        console.log(typeof(this.newDocName), this.newDocStart, this.newDocEnd);
+    }
 }
