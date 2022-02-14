912f7ad5317d62208976149a67c2a66f1deae318
IvanSkrebatovich <ivan.skrebatovich@gmail.com>
2022-02-14 14:35:41 +0300

    Update
    -ModelDoctor created
    -ControllerDoctor created

--- force-app/main/default/lwc/appointmentApp/appointmentApp.js
+++ force-app/main/default/lwc/appointmentApp/appointmentApp.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getDoctors from '@salesforce/apex/ControllerDoctors.getDoctors';
 
-export default class AppointmentApp extends LightningElement {}
+export default class AppointmentApp extends LightningElement {
+    @wire(getDoctors) wiredDoctors;
+    value = 'choose Doctor';
+
+    get options() {
+        return [
+            { label: 'Doctor1', value: 'Doctor1' },
+            { label: 'Doctor2', value: 'Doctor2' },
+            { label: 'Doctor3', value: 'Doctor3' },
+            
+        ];
+    }
+
+    handleChange(event) {
+        this.value = event.detail.value;
+    }
+}
